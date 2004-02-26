b1c51d42211f715043a3252550ef2fde1a3065e5
Egbert Eich <eich@suse.de>
2004-02-26 13:35:54 +0000

    readding XFree86's cvs IDs

--- src/smi_video.c
+++ src/smi_video.c
@@ -41,7 +41,7 @@ Author of changes: Corvin Zahn <zahn@zac.de>
 Date:   2.11.2001
 */
 
-/* $XFree86$ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/siliconmotion/smi_video.c,v 1.14 2003/12/08 16:03:58 alanh Exp $ */
 
 #include "smi.h"
 #include "smi_video.h"
